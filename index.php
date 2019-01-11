bc66973fd4fd9ff2fe8d111d5e56d8070ada6075
aaronweggemans <aaronweggemans@hotmail.nl>
2019-01-11 16:17:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="assets/css/css.css">
+</head>
+<body>
+<div class="full-size">
+    <div class="line">
+        <a href="https://adsd.clow.nl/~s1130601/P2_Laravel_Opdracht/">
+            <div class="float project filter">
+                <div class="centering">
+                    <div class="border">
+                        <p>Laravel Boardgame App</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="https://adsd.clow.nl/~s1130601/P2_NodeJS_Opdracht/80/">
+            <div class="float project2 filter">
+                <div class="centering">
+                    <div class="border">
+                        <p>NodeJS Weather App</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="https://adsd.clow.nl/~s1130601/P1_OOAPP_Opdracht/">
+            <div class="float project3 filter">
+                <div class="centering">
+                    <div class="border">
+                        <p>Boardgame app</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="https://adsd.clow.nl/~s1130601/P1_OOAPP_Tentamen/login">
+            <div class="float project4 filter">
+                <div class="centering">
+                    <div class="border">
+                        <p>PHP tentamen Applicatie</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="https://adsd.clow.nl/~s1130601/P2_NodeJS_Opdracht/81/">
+            <div class="float project5 filter">
+                <div class="centering">
+                    <div class="border">
+                        <p>NodeJS Taken App</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
+</body>
+</html>
